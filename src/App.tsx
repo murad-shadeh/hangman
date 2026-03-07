e52b178354524d3014a0da59a9d56ff42e09e78f
murad-dabbous <moradyousef04@gmail.com>
2026-03-07 01:53:45 +0000

    feat(drawing): add the hangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import words from "./data/wordList.json";
+import HangmanDrawing from "./components/HangmanDrawing";
+import HangmanWord from "./components/HangmanWord";
+import Keyboard from "./components/Keyboard";
 function App() {
   const [wordToGuess, setWordToGuess] = useState<string>(() => {
     // get random word from the list of words no matter the number
@@ -7,7 +10,23 @@ function App() {
   });
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   console.log(wordToGuess);
-  return <h1>Hello, Hangman!</h1>;
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,129 @@
+// we need to draw the hangman based on the typed letters
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+);
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      backgroundColor: "black",
+      position: "absolute",
+      //   NEED IT BELOW THE HEAD 120PX
+      top: "120px",
+      right: "0",
+    }}
+  />
+);
+const RIGHT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      backgroundColor: "black",
+      position: "absolute",
+      top: "150px",
+      right: "-100px",
+      rotate: "-30deg",
+      //   make the rotation point the left end of the arm
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+const LEFT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      backgroundColor: "black",
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+const RIGHT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      backgroundColor: "black",
+      position: "absolute",
+      //   need it at the bottom of the body 210px
+      top: "210px",
+      right: "-90px",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+const LEFT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      backgroundColor: "black",
+      position: "absolute",
+      top: "210px",
+      right: "0px",
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+export default function HangmanDrawing() {
+  return (
+    <div style={{ position: "relative" }}>
+      {/* // drawing the bottom lines */}
+      {/* divs must stay in certain order */}
+      {HEAD}
+      {BODY}
+      {RIGHT_ARM}
+      {LEFT_ARM}
+      {RIGHT_LEG}
+      {LEFT_LEG}
+      <div
+        style={{
+          height: "50px",
+          width: "10px",
+          backgroundColor: "black",
+          marginLeft: "120px",
+          top: "0",
+          right: "0",
+          position: "absolute",
+        }}
+      />
+      <div
+        style={{
+          height: "10px",
+          width: "200px",
+          backgroundColor: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div
+        style={{
+          height: "400px",
+          width: "10px",
+          backgroundColor: "black",
+          marginLeft: "120px",
+        }}
+      />
+      <div
+        style={{ height: "10px", width: "250px", backgroundColor: "black" }}
+      />
+    </div>
+  );
+}
