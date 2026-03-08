5e4b9aee2192a5738de9690f6f56de0ee491fd52
murad-dabbous <moradyousef04@gmail.com>
2026-03-08 22:07:55 +0000

    feat(hangmanword): add the hangman word component

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,3 +1,33 @@
 export default function HangmanWord() {
-  return <div>HangmanWord</div>;
+  const word = "test";
+  const guessedLetters = ["t", "a", "n"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25rem",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {/* we need to render each character individually */}
+      {word.split("").map((letter, index) => (
+        // for each letter we need to put a certain style underneath it
+        <span key={index} style={{ borderBottom: ".1em solid black" }}>
+          <span
+            // show letter if it is guessed otherwise hide it
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? "visible"
+                : "hidden",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
 }
